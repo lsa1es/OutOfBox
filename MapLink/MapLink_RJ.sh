2f15a8a51b7b66d2ccc7b75c031be7adacd9570f
Luiz Sales <lsa1es@users.noreply.github.com>
2016-07-06 23:16:20 -0300

    Create MapLink_RJ.sh

---
+++ MapLink/MapLink_RJ.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Script para buscar dados do MapLink com relação ao transito total.
+# v0.1 - Luiz Sales
+# www.redhate.me - www.lsales.biz
+#
+# GPL 2 - Se for usar o codigo ou copiar em outra linguagem, favor mencioar o autor e o site.
+#
+
+#VARIAVEIS
+ZABBIX_SERVER="127.0.0.1"
+
+URL="http://maplink.com.br/RJ/rio_de_janeiro/TransitoAgora"
+URLF="rjagora.html"
+
+lynx -source $URL > $URLF
+total_transito=$(cat $URLF | grep "totalSlow" | awk -F\> '{print $3}' | sed 's/<\/b//g')
+while [ -z "$total_transito" ]; do
+        cont_ts=`cat $URLF | grep "totalSlow" | wc -l`
+        if [ "$cont_ts" -le "1" ]; then
+                lynx -source $URL > $URLF
+                total_transito=$(cat $URLF | grep "quilometroTransito" | awk -F\> '{print $2}' | sed 's/km<\/strong//g')
+                else
+                lynx -source $URL > $URLF
+                total_transito=$(cat $URLF | grep "totalSlow" | awk -F\> '{print $3}' | sed 's/<\/b//g')
+        fi
+done
+
+/usr/local/zabbix/bin/zabbix_sender  -z $ZABBIX_SERVER -s Maplink_RJ -k tr.total -o $total_transito
+
+
